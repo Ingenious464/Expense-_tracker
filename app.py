525689c60ef2feaf528500208caf2321132f2e02
Adetunji Fatai <sincerityisgood@gmail.com>
2023-12-17 17:17:40 +0100

    templates

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+expenses = []
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        amount = float(request.form['amount'])
+        category = request.form['category']
+        expenses.append({'amount': amount, 'category': category})
+    return render_template('index.html', expenses=expenses)
 
 if __name__ == '__main__':
     app.run(debug=True)
